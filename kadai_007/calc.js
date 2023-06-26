c99c4d2b4f27d13a371009a16b76883fcea1d785
tunamayo0818 <129466973+tunamayo0818@users.noreply.github.com>
2023-06-27 01:54:51 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,35 @@
+let num = 15;
+
+switch (num) {
+  case 3:
+    console.log('3の倍数です');
+    break;
+    
+  case 6:
+    console.log('3の倍数です');
+    break;
+  
+  case 9:
+    console.log('3の倍数です');
+    break;
+
+  case 12:
+    console.log('3の倍数です');
+    break;
+    
+  case 5:
+    console.log('5の倍数です');
+    break;
+
+  case 10:
+    console.log('5の倍数です');
+    break;  
+    
+  case 15:
+    console.log('3と5の倍数です');
+    break;
+   
+  default:
+    console.log(num);
+    break;
+}
